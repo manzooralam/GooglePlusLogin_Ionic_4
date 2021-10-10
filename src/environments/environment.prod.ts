52746808c1540e7bd0c866cb172ad49469448c3b
Manzoor Alam <40365917+manzooralam@users.noreply.github.com>
2021-10-10 23:25:37 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,  
   googleWebClientId : 'AIzaSyCAd0.apps.googleusercontent.com',
   firebase: {
-    apiKey: "AIzaSyCP3NN-2iIQAHYowK2Ad0",
+    apiKey: "AIzaSyCP*******2iIQAHYowK2Ad0",
     authDomain: "ionicd65.firebaseapp.com",
-    databaseURL: "https://ionicdemo-78065.firebaseio.com",
+    databaseURL: "https://ionicdemo-8065.firebaseio.com",
     projectId: "ionic5",
-    storageBucket: "ionicdemo-78065.appspot.com",
+    storageBucket: "ionicdemo-8065.appspot.com",
     messagingSenderId: "440368420102",
     appId: "1:440368420102:web:aa0d3767faaef7d1"
   }
